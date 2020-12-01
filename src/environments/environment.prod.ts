8f71a7c14175dcbe3cc608286dfd9bb0be6a4070
Paul Chesa <paulchesa1@gmail.com>
2020-12-01 05:18:50 +0300

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
+
 export const environment = {
-  production: true,
-  firebaseConfig :{
+   production: true,
+   firebaseConfig :{
     apiKey: process.env.API_KEY,
     authDomain:  process.env.AUTH_DOMAIN,
     databaseURL:  process.env.DATABASE_URL,
-    projectId: "excalibur-95b6a",
-    storageBucket:  process.env.STORAGE_BUCKET,
+    projectId:  "excalibur-95b6a",
+    storageBucket:  "excalibur-95b6a.appspot.com",
     messagingSenderId:  process.env.MESSAGING_SENDER_ID,
     appId:  process.env.API_ID,
     measurementId:  process.env.MEASUREMENT_ID
-  }
-};
+}
+}
